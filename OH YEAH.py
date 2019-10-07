efd50cd62dd11acaaf7a73b1622d156df8c1dae3
Christopher-Nall <bigcmn@gmail.com>
2019-10-07 15:28:02 -0500

    OH YEAH

--- OH YEAH.py
+++ OH YEAH.py
@@ -4,6 +4,20 @@ x = "there are %d types of people." % 10
 binary = "binary"
 doNot = "don't"
 y = "Those who know %s and those who %s" % (binary, doNot)
+
 print(x)
 print(y)
+
+print("I said: %r.:" %x)
+print("I also said: '%s'" % y)
+
+hilarious = True
+jokeEvaluation = "Isn't that joke so funny?! %r"
+
+w = "This is the left side of..."
+e = "a string with a right side"
+print(w+e)
+
+# More Printing fun
+
 
