3571c475195942b9c10933f8f72e87f901d2f6a9
Christopher-Nall <bigcmn@gmail.com>
2019-10-07 15:39:10 -0500

    OH YEAH

--- OH YEAH.py
+++ OH YEAH.py
@@ -20,4 +20,23 @@ print(w+e)
 
 # More Printing fun
 
+print("Mary had a little lamb.")
+print("its fleece was white as %s." % 'snow')
+print("and everywhere that mary went.")
+print("." * 10)
+end1 = "c"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+end5 = "s"
+end52 = "e"
+end6 = "b"
+end7 = "u"
+end8 = "r"
+end9 = "g"
+end10 = "e"
+end11 = "r"
+
+print(end1+end2+end3+end4+end5+end52)
+print(end6+end7+end8+end9+end10+end11)
 
