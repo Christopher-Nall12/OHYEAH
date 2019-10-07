759be8532f69e6f3b433d30faa8a7057347b5621
Christopher-Nall <bigcmn@gmail.com>
2019-10-07 15:19:03 -0500

    OH YEAH

---
+++ OH YEAH.py
@@ -0,0 +1,9 @@
+# more strings and text
+
+x = "there are %d types of people." % 10
+binary = "binary"
+doNot = "don't"
+y = "Those who know %s and those who %s" % (binary, doNot)
+print(x)
+print(y)
+
