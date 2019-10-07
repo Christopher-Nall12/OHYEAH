9ca5c014d355fdff3d13ebcc91e02eab9110b660
Christopher-Nall <bigcmn@gmail.com>
2019-10-07 15:50:10 -0500

    CHEESEBURGER

--- OH YEAH.py
+++ OH YEAH.py
@@ -1,15 +1,23 @@
 # more strings and text
 
+# i am setting the shortcuts that will help me to type stories or equations
+
 x = "there are %d types of people." % 10
 binary = "binary"
 doNot = "don't"
 y = "Those who know %s and those who %s" % (binary, doNot)
 
+# i used the shortcut i made to tell the computer to type something like what i told it to
+
 print(x)
 print(y)
 
+# i used the shortcut words/signs to type out a statement easier
+
 print("I said: %r.:" %x)
 print("I also said: '%s'" % y)
+
+# i am setting the words i dont want to type out as a smaller easier word to type
 
 hilarious = True
 jokeEvaluation = "Isn't that joke so funny?! %r"
@@ -19,11 +27,12 @@ e = "a string with a right side"
 print(w+e)
 
 # More Printing fun
-#
+# I am telling the computer to write these things though the word PRINT
 
 print("Mary had a little lamb.")
 print("its fleece was white as %s." % 'snow')
 print("and everywhere that mary went.")
+# in this line i am telling the computer to say the word/phrase/symbol withing the "" to say it 10 times or multiply it by 10.
 print("." * 10)
 
 #below i am setting each end into a letter
